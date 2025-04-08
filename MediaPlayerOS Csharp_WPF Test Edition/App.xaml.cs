426a6f4bbb7d7f2fd56c624e58a9546e05f2a598
1510ty <1510ty2580@gmail.com>
2025-04-08 10:23:00 +0900

    スタートボタンとシャットダウンボタンを実装。

---
+++ MediaPlayerOS Csharp_WPF Test Edition/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MediaPlayerOS_Csharp_WPF_Test_Edition
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MediaPlayerOS Csharp_WPF Test Edition/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayerOS_Csharp_WPF_Test_Edition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStyle = WindowStyle.None;      // タイトルバー非表示
+            this.ResizeMode = ResizeMode.NoResize;    // リサイズ不可
+            this.WindowState = WindowState.Maximized; // 最大化（画面いっぱい)
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (StartMenu.Visibility == Visibility.Visible)
+            {
+                StartMenu.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                StartMenu.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ShutdownButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
